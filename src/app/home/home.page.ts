1a01ef65c44ced4c7292b0d9a7baf3e7fdcbf0e2
Calvin Rodo <calvin.rodo@hrsdc-rhdcc.gc.ca>
2018-11-12 20:15:10 -0500

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  common = new Audio("./assets/Fart-Common-Everyday-Fart_Mike-Koenig.mp3");
+  quick = new Audio("./assets/Quick Fart-SoundBible.com-655578646.mp3");
+  silly = new Audio("./assets/Silly_Farts-Joe-1473367952.mp3");
+  mower = new Audio("./assets/Lawn Mower Fart-SoundBible.com-771525987.mp3");
+  raspberry = new Audio("./assets/Uuuuuu-Paula-1357936016.mp3");
+
+  constructor(){
+
+  }
+
+  commonFart(){
+    this.common.play();
+  }
+
+  quickFart(){
+    this.quick.play();
+  }
+
+  sillyFart(){
+    this.silly.play();
+  }
+
+  mowerFart(){
+    this.mower.play();
+  }
+
+  raspberryFart(){
+    this.raspberry.play()
+  }
+
+
+
+
 }
